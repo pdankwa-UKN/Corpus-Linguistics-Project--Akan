1f5a922a7b898c60e649ca3bce2df459e0bc34b5
pdankwa-UKN <p.afaribeadankwa@gmail.com>
2026-08-31 09:49:13 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(
     name="akcorp_package",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=["requests", "beautifulsoup4", "trafilatura", "lxml_html_clean","regex"],)
+    install_requires=["requests", "beautifulsoup4", "trafilatura", "lxml", "lxml_html_clean","regex"],)
