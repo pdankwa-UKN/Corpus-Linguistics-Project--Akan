2288f0ec7162f333ec8707546be99d89cb91ba6e
pdankwa-UKN <p.afaribeadankwa@gmail.com>
2026-09-14 05:31:55 +0200

    Update _init_.py

--- akcorp_package/_init_.py
+++ akcorp_package/_init_.py
@@ -1,1 +1,1 @@
-from .corpli import scrape_verses, extract_metadata, segment_and_tokenize_verses, create_clean_verses, corpus_search, comprehensive_analysis, corpus_analysis
+from .corpli import scrape_verses, extract_metadata, segment_and_tokenize_verses, create_clean_verses, save_stopwords, train_pos_tagger, corpus_search, comprehensive_analysis, corpus_analysis
